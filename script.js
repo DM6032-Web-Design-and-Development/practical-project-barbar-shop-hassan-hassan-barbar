c29bc98cf624210b997f1bcfcb8544633b541203
web6641-lang <web6641@gmail.com>
2025-12-03 17:12:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector(".book-btn").addEventListener("click", function () {
   alert("Booking system coming soon!");
 });
+function order(productName) {
+let link = "https://wa.me/37128918?text=I want to order: " + encodeURIComponent(productName);
+window.open(link, "_blank");
+}
