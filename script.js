9b95b6aa35801f980b266fbedd69f9eba6410d54
Shehabi76 <202304078@student.polytechnic.bh>
2025-12-08 00:31:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector(".book-btn").addEventListener("click", function () {
-  alert("Booking system coming soon!");
+
 });
 function order(productName) {
 let link = "https://wa.me/37128918?text=I want to order: " + encodeURIComponent(productName);
