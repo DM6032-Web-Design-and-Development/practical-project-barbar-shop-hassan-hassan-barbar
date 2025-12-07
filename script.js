259d52aa683af4af0f75dbf6a866f6e4b27a0643
Shehabi76 <202304078@student.polytechnic.bh>
2025-12-08 00:17:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function order(productName) {
 let link = "https://wa.me/37128918?text=I want to order: " + encodeURIComponent(productName);
 window.open(link, "_blank");
 }
+document.getElementById("bookingForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Form submitted successfully!");
+})
